e46942da8ad2b59a56f6572d4ba2d82e196b865c
小郭 <1349224063@qq.com>
2018-01-11 15:53:21 +0800

    change

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+import 'assets/css/reset.css'
 
+FastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
