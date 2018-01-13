42a8f1615d663abd530e04217e716397152aa2c0
1056253401@qq.com <you@example.com>
2018-01-13 17:56:55 +0800

    change

---
+++ src/pages/index/header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="back iconfont">&#xe6ba;</div>
+    <div class="search">
+      <span class="iconfont search-icon">&#xe998;</span>
+      请输入搜素内容
+    </div>
+    <div class="city">
+      乌鲁木齐
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index-header'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../assets/stylus/varibles.styl";
+  .header
+    display: flex
+    height: .88rem
+    line-height: .88rem
+    background: $bgColor
+    color: #fff
+    .back
+      width: .58rem
+      text-align: center
+      font-size: .4rem
+      font-weight: bold
+    .search
+      flex: 1
+      height: .6rem
+      line-height: .6rem
+      margin: .14rem .2rem
+      padding-left: .4rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .city
+      padding-right: .6rem
+      position: relative
+    &:before
+      position: absolute
+      top: .34rem
+      right: .2rem
+      content: "\0020"
+      width: 0
+      height: 0
+      border-left: .14rem solid transparent
+      border-right: .14rem solid transparent
+      border-top: .14rem solid #fff
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-    index
+    <index-header></index-header>
+    <index-swiper :list="swiperInfo"></index-swiper>
+    <index-icons :list="iconsInfo"></index-icons>
+    <index-hotshow :list="hotshowInfo"></index-hotshow>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'index'
-}
+  import IndexHeader from './header'
+  import IndexSwiper from './swiper'
+  import IndexIcons from './icons'
+  import IndexHotshow from './hotshow'
+  import axios from 'axios'
+  export default {
+    name: 'index',
+    components: {
+      IndexHeader,
+      IndexSwiper,
+      IndexIcons,
+      IndexHotshow
+    },
+    data () {
+      return {
+        swiperInfo: [],
+        iconsInfo: [],
+        hotshowInfo: []
+      }
+    },
+    methods: {
+      getIndexData () {
+        axios.get('/api/index.json')
+          .then(this.handleGetDataSucc.bind(this))
+          .catch(this.handleGetDataErr.bind(this))
+      },
+      handleGetDataSucc (res) {
+        const data = res.data.data
+        this.swiperInfo = data.swiperList
+        this.iconsInfo = data.iconList
+        this.hotshowInfo = data.hotshowList
+      },
+      handleGetDataErr () {
+        console.log('error')
+      }
+    },
+    created () {
+      this.getIndexData()
+    }
+  }
 </script>
 
-<style>
-
-</style>
+<style></style>
